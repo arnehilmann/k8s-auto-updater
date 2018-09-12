72ff41193280682b5bc234ebe2979ed445a436f0
Arne Hilmann <arne@hilmann.de>
2018-09-12 14:53:49 +0200

    improve log output and layout

--- src/autoupdater/rollingupdate.py
+++ src/autoupdater/rollingupdate.py
@@ -12,20 +12,20 @@ from . import (header,
                check_pods)
 
 
-def rolling_update_on_deployment(pod, pod_name, repodigest, **args):
+def rolling_update_on_deployment(pod, pod_name, repodigest, patched={}, verbose=False, **args):
     '''
     strategy for check_pods
     '''
-    print(args.keys())
+    # print(args.keys())
 
     replica_set = get_first_owner(pod)
     if not replica_set:
-        print("\tno owning replica set found for pod/{}, strange!".format(pod_name))
+        print("\t\tno owning replica set found for pod/{}, strange!".format(pod_name))
         return False
 
     deployment = get_first_owner(replica_set)
     if not deployment:
-        print("\tno owning deployment found for replicaset/{}, strange!".format(
+        print("\t\tno owning deployment found for replicaset/{}, strange!".format(
             replica_set["metadata"]["name"]))
         return False
 
@@ -33,10 +33,10 @@ def rolling_update_on_deployment(pod, pod_name, repodigest, **args):
         repodigest, deployment["metadata"]["name"]))
     raw_result = subprocess.run([
         "kubectl", "set", "env",
-        "deployment/{}".format(deployment["metadata"]["name"]),
+        "deployment/{}".format(deployment_name),
         "newestrepodigest={}".format(repodigest)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if raw_result.returncode != 0:
-        print("\t[WARN] {}".format(raw_result.stderr))
+        print("\t\t[WARN] {}".format(raw_result.stderr))
         return False
 
     return True
@@ -46,20 +46,22 @@ def run():
     '''
     actually run the check with the rolling_update_on_deployment strategy
     '''
-    header("auto-updater", "=")
-    print(".\nsyncing pods and images against local and remote registry\n")
+    header("k8s-auto-updater", "=")
+    print("\nsyncing pods and images against local and remote registry")
+    print("(see https://github.com/arnehilmann/k8s-auto-updater)\n")
 
     image_regexp = os.getenv("IMAGE_REGEXP", ".*")
     pod_selectors = os.getenv("POD_SELECTOR", "auto-update=enabled")
     verbose = str(os.getenv("VERBOSE", "")).lower() in ("true", "yes", "1")
 
     header("fetching pods, current repodigest and image name")
-    print("\timage regexp: {}".format(image_regexp))
-    print("\tpod selectors: {}".format(pod_selectors))
+    print("IMAGE_REGEXP: {}".format(image_regexp))
+    print("POD_SELECTOR: {}".format(pod_selectors))
+    print("     VERBOSE: {}".format(verbose))
 
     data = collect_data(image_regexp, pod_selectors, verbose)
 
-    header("checking remote repositories, deleting outdated pods")
+    header("checking remote repositories, patching deployments of outdated pods")
     check_pods(data, rolling_update_on_deployment, verbose)
 
-    print(".\ndone.")
+    print("\ndone.")
